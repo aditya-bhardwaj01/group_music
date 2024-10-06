1a9d3d458dad2d7a1eb0595b15622c72676ec484
aditya-bhardwaj01 <aditya.bhardwaj2020@vitstudent.ac.in>
2024-10-06 19:39:32 +0530

    connection file updated

--- server/database/Connection.js
+++ server/database/Connection.js
@@ -1,10 +1,16 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    password: "root",
-    database: "group_music"
+    // user: "root",
+    // host: "172.17.0.2",
+    // password: "root",
+    // database: "group_music",
+
+    // if we are using docker compose
+    user: process.env.MYSQL_USER || "root",
+    host: process.env.MYSQL_HOST || "localhost",
+    password: process.env.MYSQL_PASSWORD || "root",
+    database: process.env.MYSQL_DATABASE || "group_music",
 });
 
 module.exports = db
